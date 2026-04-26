3cacb9155fff9b5539f26c64386758bca77e5a66
Virendra Gayakwad <62784876+Viren-hub@users.noreply.github.com>
2026-04-26 20:16:54 +0530

    Delete backend directory

--- backend/src/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect, authorize } = require('../middleware/auth');
-const { getDashboard } = require('../controllers/adminDashboardController');
-
-router.get('/dashboard', protect, authorize('admin'), getDashboard);
-
-module.exports = router;
